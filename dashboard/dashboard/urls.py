efc4757a706b96c38c52146ebca4be121ccf5519
laosong-2020 <songzl0606@gmail.com>
2022-07-04 17:25:51 -0500

    change path

--- dashboard/dashboard/urls.py
+++ dashboard/dashboard/urls.py
@@ -19,7 +19,6 @@ from django.conf.urls import url
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('map/', include('apps.maps.urls')),
+    path('', include('apps.maps.urls')),
     path('admin/', admin.site.urls),
-    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
 ]
